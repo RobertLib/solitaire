cdd76ebd6bab551c2826b131758fc97350f7ac39
RobertLib <rob@hotmail.cz>
2026-08-25 20:32:37 +0200

    Initial Commit

---
+++ solitaire/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  solitaire
+//
+//  Created by Robert Libšanský on 25.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ solitaire/solitaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  solitaireApp.swift
+//  solitaire
+//
+//  Created by Robert Libšanský on 25.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct solitaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
